531c41f179f36f0229b99ab092afcf46e63d49fa
Maheen Khalid <maheenkhalid08@gmail.com>
2016-08-29 19:20:38 +0500

    Complete code for UITableView in Swift

--- UITableView - Tutorial/ViewController.swift
+++ UITableView - Tutorial/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableview.delegate = self
+        tableview.dataSource = self
+        
+        tableview.registerClass(UITableViewCell.self, forCellReuseIdentifier: "CELL");
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: - UITableViewDataSource Methods -
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CELL", forIndexPath: indexPath)
+        
+        cell.textLabel?.text = "Cell is: \(indexPath.row)"
+        
+        return cell
+    }
+    
+    //MARK: - UITableViewDelegate Methods -
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 64
+    }
 
 }
 
